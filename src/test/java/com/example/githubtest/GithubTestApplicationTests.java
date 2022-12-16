601354b3811bc52dc4ccbe53d099253e2d93af84
yeonini <leeyeon.cd@gmail.com>
2022-12-16 16:56:26 +0900

    initial

---
+++ src/test/java/com/example/githubtest/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
